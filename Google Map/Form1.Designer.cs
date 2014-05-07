48bd5d58610d0be592aa69c762024e5bbf47ddb2
ZenunVee <zenunv@aol.com>
2014-05-07 10:39:05 -0400

    Added a track bar for the Street View to change the heading. Each position in the track bar changes the heading by 45 degrees. Also changed that whenever you scroll on either track bar, you do not need to press the "Static Map" or the "Street View" button. The map changes automatically. Also changed the color of some buttons and made labels transparent.

--- Google Map/Form1.Designer.cs
+++ Google Map/Form1.Designer.cs
@@ -36,9 +36,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -48,7 +51,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(353, 267);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox2
             // 
@@ -57,7 +59,6 @@
             this.pictureBox2.Size = new System.Drawing.Size(353, 268);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // staticBtn
             // 
@@ -72,6 +73,7 @@
             // 
             // strtBtn
             // 
+            this.strtBtn.FlatAppearance.BorderSize = 2;
             this.strtBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.strtBtn.Location = new System.Drawing.Point(371, 210);
             this.strtBtn.Name = "strtBtn";
@@ -118,6 +120,25 @@
             this.label2.TabIndex = 11;
             this.label2.Text = "Zoom";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(502, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Heading (Angle)";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(503, 236);
+            this.trackBar2.Maximum = 8;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(180, 45);
+            this.trackBar2.TabIndex = 14;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -125,6 +146,8 @@
             this.BackgroundImage = global::Google_Map.Properties.Resources.google_maps_logo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(734, 555);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label1);
@@ -134,11 +157,12 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Google Maps";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,6 +178,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
     }
 }
 
--- Google Map/Form1.cs
+++ Google Map/Form1.cs
@@ -29,16 +29,8 @@ namespace Google_Map
 
         string address;
         string zoom;
+        int heading;
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -61,7 +53,7 @@ namespace Google_Map
 
         private void strtBtn_Click(object sender, EventArgs e)
         {
-            requestPicStreet = WebRequest.Create("http://maps.googleapis.com/maps/api/streetview?location=" + address + "&heading=0&zoom=13&size=453x267&sensor=false");
+            requestPicStreet = WebRequest.Create("http://maps.googleapis.com/maps/api/streetview?location=" + address + "&heading=" + heading.ToString() + "&zoom=12&size=453x267&sensor=false");
             repsonsePicStreet = requestPicStreet.GetResponse();
             mapStreet = Image.FromStream(repsonsePicStreet.GetResponseStream());
             pictureBox2.Image = mapStreet;
@@ -70,6 +62,26 @@ namespace Google_Map
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             zoom = trackBar1.Value.ToString();
+            requestPicStatic = WebRequest.Create("http://maps.googleapis.com/maps/api/staticmap?center=" + address + "&zoom=" + zoom + "&size=353x267&sensor=false");
+            repsonsePicStatic = requestPicStatic.GetResponse();
+            mapStatic = Image.FromStream(repsonsePicStatic.GetResponseStream());
+            pictureBox1.Image = mapStatic;
         }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if(trackBar2.Value == 0)
+                heading = 45 + trackBar2.Value;
+
+            if (trackBar2.Value != 0)
+                heading = 45 + heading;
+
+            requestPicStreet = WebRequest.Create("http://maps.googleapis.com/maps/api/streetview?location=" + address + "&heading=" + heading.ToString() + "&size=353x267&sensor=false");
+            repsonsePicStreet = requestPicStreet.GetResponse();
+            mapStreet = Image.FromStream(repsonsePicStreet.GetResponseStream());
+            pictureBox2.Image = mapStreet;
+        }
+
+        
     }
 }
